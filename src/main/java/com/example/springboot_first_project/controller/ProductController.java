4769c59775886760d2a4eb495c086532821737ce
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2025-11-14 13:49:44 -0300

    feat. ProductController class implementation

---
+++ src/main/java/com/example/springboot_first_project/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.example.springboot_first_project.controller;
+
+import com.example.springboot_first_project.model.Product;
+import com.example.springboot_first_project.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        return productService.getProduct(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product saveProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
